7ff1686edaebf4845b1b20f3c39514171e2748b8
Oyewo Temidayo <oyewodayo@gmail.com>
2022-12-05 14:05:18 +0100

    Initial commit to greg.

---
+++ target/debug/deps/greg-695d85a1dfdb27dc.d
@@ -0,0 +1,5 @@
+c:\Users\HP\Desktop\Rust\greg\target\debug\deps\greg-695d85a1dfdb27dc.rmeta: src\main.rs
+
+c:\Users\HP\Desktop\Rust\greg\target\debug\deps\greg-695d85a1dfdb27dc.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/greg-a550b3e9c78da0e7.d
@@ -0,0 +1,5 @@
+c:\Users\HP\Desktop\Rust\greg\target\debug\deps\greg-a550b3e9c78da0e7.rmeta: src\main.rs
+
+c:\Users\HP\Desktop\Rust\greg\target\debug\deps\greg-a550b3e9c78da0e7.d: src\main.rs
+
+src\main.rs:
